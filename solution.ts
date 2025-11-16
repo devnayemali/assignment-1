33fc2b218fc4192a01fd78e9b9ee10323204cb2d
Md Nayem Ali <106856439+devnayemali@users.noreply.github.com>
2025-11-16 23:49:47 +0600

    problem 2 done

--- solution.ts
+++ solution.ts
@@ -12,3 +12,17 @@ const formatValue = (value: string | number | boolean): string | number | boolea
 
     return value
 };
+
+
+const getLength = (data: string | any[]): number => {
+
+    if (typeof data === "string") {
+        return data.length;
+    } else if (Array.isArray(data)) {
+        return data.length;
+    } else {
+        return 0;
+    }
+
+}
+
